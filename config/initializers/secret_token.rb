32dad55874111177989b518be58897ab5e173198
Christopher Young-Zawada <cyoungzawada@gilt.com>
2012-02-15 00:18:35 -0500

    Updated project with a rails app for demoing, as well as the uploading the api resource itself

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GiltJsApi::Application.config.secret_token = '71d3fcafb9ccf8427b6f7e60b84219e94a7f2a073358881bc868b2b7b1098b4bd0d38eb7629119b5197ef13ead1d9a202a139b013a670dab2c5c20f2a3a50cd0'
---
+++ public/javascripts/test.js
@@ -0,0 +1,121 @@
+($(function() {
+  
+  var submitKey = $( '#submitkey' ),
+      keyField = $( '#apikey' ),
+      saleHolder = $( '#sale-holder' ),
+      productHolder = $( '#product-holder' );
+      
+  var api,
+      didInitAccordion = false;
+  
+  function getImageUrl( sale ) {
+    //get first imageUrl
+    for( key in sale.image_urls ) {
+      return sale.image_urls[ key ][ 0 ].url;
+    }
+  }
+  
+  function getProductViewHTML( sale ) {
+    if( sale.products && sale.products.length > 0 ) {
+      return '<div><a class="load-products" href="#">View Products</a></div>';
+    } else {
+      return '';
+    }
+  }
+  
+  function getDescriptionHTML( item, isProduct ) {
+    var desc = isProduct ? item.content.description : item.description;
+    if( desc && desc.length > 0 ) {
+      return '<div><p>' + desc +'</p></div>';
+    } else {
+      return '';
+    }
+  }
+  
+  keyField.keyup(function() {
+    if( $( this ).val().length > 0 ) {
+      submitKey.fadeIn();
+    } else {
+      submitKey.fadeOut();
+    }
+  });
+  
+  submitKey.click(function() {
+    apiKey = keyField.val();
+    api = new GiltApi( apiKey );
+    
+    $( '#init' ).fadeOut(function() {
+      $( '#content' ).fadeIn();
+    });
+    
+  });
+  
+  $( '#getcurrent' ).click(function() {
+    var content = $( '<div class="accordion"></div>' );
+    api.getSales( function( sales ) {
+      $.each( sales, function() {
+        var child = $(
+          '<h3 class="sale-item"><a href="#">' + this.name + '</a></h3>' +
+          '<div>' +
+            getProductViewHTML( this ) +
+            '<div><img src="' + getImageUrl( this ) + '"/></div>' +
+            getDescriptionHTML( this ) +
+          '</div>'
+        );
+        
+        child.find( 'a' ).data( this );
+        content.append( child );
+      });
+      saleHolder.html( content );
+      productHolder.empty();
+      content.accordion();
+    });
+  });
+  
+  $( '#getupcoming' ).click(function() {
+    var content = $( '<div class="accordion"></div>' );
+    api.getSales( function( sales ) {
+      $.each( sales, function() {
+        content.append(
+          '<h3 class="sale-item"><a href="#">' + this.name + '</a></h3>' +
+          '<div>' +
+            '<div>Begins at: <strong>' + this.begins + '</strong></div>' +
+            '<div><img src="' + getImageUrl( this ) + '"/></div>' +
+            getDescriptionHTML( this ) +
+          '</div>'
+        )
+      });
+      saleHolder.html( content );
+      productHolder.empty();
+      content.accordion();
+    }, true);
+  });
+  
+  $( '.load-products' ).live( 'click', function() {
+    var content = $( '<div class="accordion"></div>' ),
+        products = $( this ).data().products;
+    
+    var dfds = [];
+    
+    $.each( products, function() {
+      dfds.push(
+        api.getProductDetail( this, function( product ) {
+          content.append(
+            '<h3 class="product-item"><a href="#">' + product.name + '</a></h3>' +
+            '<div>' +
+              '<div><strong>' + product.brand + '</strong></div>' +
+              '<div><a target="_blank" href="' + product.url + '">Buy it now!</a></div>' +
+              '<div><img src="' + getImageUrl( product ) + '"/></div>' +
+              getDescriptionHTML( product, true ) +
+            '</div>'
+          )
+        })
+      );
+    });
+    $.when.apply($, dfds).then(function() {
+      productHolder.html( content );
+      content.accordion();
+    });
+  });
+  
+}));
